50a58c42534927958f6ef6bbacbd8a3cc552b013
Aleksandra <alex.peterson@example.com>
2026-06-23 19:06:29 +0200

    promena

--- Introduction to Python Programming - termin 6/primerRange.py
+++ Introduction to Python Programming - termin 6/primerRange.py
@@ -1,5 +1,3 @@
 brojevi = reversed(range(0,9))
 print(list(brojevi))
-
-
-# napisati poziv funkcije range tako da vraca brojeve 9 7 5 3 1
+# napisati poziv funkcije range tako da vraca brojeve 9 7 5 3 1# napisati poziv funkcije range tako da vraca brojeve 9 7 5 3 1
